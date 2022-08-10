118241b30b6000ecfc93d6fab6952d1bcabb6bbd
Ákos Szarvák <akos.szarvak@gmail.com>
2022-08-10 13:11:56 +0200

    finished crud functionallity

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,8 +6,9 @@ const {
   updateGoal,
   deleteGoal,
 } = require("../controllers/goalController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").delete(deleteGoal).put(updateGoal);
+router.use(protect).route("/").get(getGoals).post(setGoal);
+router.use(protect).route("/:id").delete(deleteGoal).put(updateGoal);
 
 module.exports = router;
